4bb70288f09e602f9ab6b53589069ccd07975860
Lucas <lguerreiroms@gmail.com>
2024-09-19 11:23:22 -0300

    Criando tipos de variaveis

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 var nome = "Fiap"
 console.log(nome)
+
+let nome1 = "cidade";
+console.log(nome1)
+
+const nome2 = "javascript"
+console.log(nome2)
+
+let nome3;
+console.log(nome3)
+
+let disciplina = null
+console.log(disciplina)
+
+let idade = 18;
+console.log(idade)
+let valor = 6.46;
+console.log(valor)
+let texto = "Fiap"
+console.log(texto)
+
+let aluno = true
+console.log(aluno)
+
+let tecnologias =["Java", "C#","HTML"]
+console.log(tecnologias)
